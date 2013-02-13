c93d28d66675742bc6ce975347bd57302344f254
Charles Wolfe <charleswolfe@gajo.us>
2013-02-13 19:44:49 +0000

    Added ability to track light status (assuming it is accurate [we cannot qury the light to see if it's on)] made web page a little more pretty for mobile

--- www/remote.js
+++ www/remote.js
@@ -1,42 +1,48 @@
 
-if (document.images)
-{
-onpressed = new Image(); onpressed.src="../images/switch_on_pressed.png";
-offpressed = new Image(); offpressed.src="../images/switch_off_pressed.png";
-onup = new Image(); onup.src="../images/switch_on_up.png";
-offup = new Image(); offup.src="../images/switch_off_up.png";
+if (document.images) {
+	onpressed = new Image(); onpressed.src="./images/switch_on_pressed.png";
+	offpressed = new Image(); offpressed.src="./images/switch_off_pressed.png";
+	onup = new Image(); onup.src="./images/switch_on_up.png";
+	offup = new Image(); offup.src="./images/switch_off_up.png";
 } 
 
-var sw1off = document.getElementById('sw1off');
-	sw1off.onclick = function (){switch_flipped("sw1off")}; 
-var sw1on = document.getElementById('sw1on');
-	sw1on.onclick = function (){switch_flipped("sw1on")}; 
-
-var sw2off = document.getElementById('sw2off');
-        sw2off.onclick = function (){switch_flipped("sw2off")};
-var sw2on = document.getElementById('sw2on');
-        sw2on.onclick = function (){switch_flipped("sw2on")};
-
- sw1off.state = 'off';
- sw1on.state = 'off';
- sw2off.state = 'off';
- sw2on.state = 'off';
-
-    function switch_flipped(who) {
-console.log("what:"+who+" ");
-
-var base_switch = who.substring(0,3);
-
-if (endsWith(who,'on')){
-console.log("turn on");
-	document[base_switch+"led"].src='images/led_on.png';
-} 
-if (endsWith(who,'off')){
-console.log("ends off");
-	document[base_switch+"led"].src='images/led_off.png';
+function switch_leds(json) {
+	obj = JSON.parse(json);
+        i =0;
+        while(obj[i]) {
+		document[obj[i].id+"led"].src='images/led_off.png';
+                if (obj[i].state == 'i') {
+                        document[obj[i].id+"led"].src='images/led_on.png';
+                }
+                i++;
+        }
 }
 
-    };
+function switch_flipped(who) {
+	var base_switch = who.substring(0,3);
+
+	if (endsWith(who,'on')){
+		console.log("turn on");
+
+	$.ajax({ url: 'http://home.gajo.us:8484/action.php',
+         data: {cmd: 'set', switch:base_switch, pos: 'on'},
+         type: 'post',
+         success: function(output) {
+			switch_leds(output);
+                  }
+		});
+	} 
+	if (endsWith(who,'off')){
+        $.ajax({ url: 'http://home.gajo.us:8484/action.php',
+         data: {cmd: 'set', switch:base_switch, pos: 'off'},
+         type: 'post',
+         success: function(output) {
+			switch_leds(output);
+                  }
+                });
+        }
+}
+
 
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
@@ -45,20 +51,23 @@ function endsWith(str, suffix) {
 
 function change(picName,imgName)
 {
-if (document.images)
-{
-imgOn=eval(imgName + ".src");
-document[picName].src= imgOn;
-}
+	if (document.images) {
+		imgOn=eval(imgName + ".src");
+		document[picName].src= imgOn;
+	}
 } 
 
-//we need to know if we are now on, or now off
-//we need to know what switch was chnaged  who
+window.onload=function(){
+	//Shopw status of lights when we first start page
+	$.ajax({ url: 'http://home.gajo.us:8484/action.php',
+         data: {cmd: 'list'},
+         type: 'post',
+         success: function(output) {
+                        switch_leds(output);
+                  }
+                });
+};
 
 
-
-//we should call the backbone, tell it
-
-//set colors from rpc return
 
 
